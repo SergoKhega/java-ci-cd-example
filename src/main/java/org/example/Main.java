9e2191eafa26aad248373f3434f924572e2d446a
Sergo Khega <hegai-sergei2012@yandex.ru>
2024-12-29 23:09:52 +0300

    1_commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        System.out.println("Result: " + result);
+    }
+}
